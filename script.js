6b7e58f619aed87cddcead08d482363b4fd6d51c
Mohammad Hussain <95843547+Hussain-ksmd@users.noreply.github.com>
2023-01-24 21:31:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // complete the given function
 
 function palindrome(str){
+    // Convert the string to lowercase
+    str = str.toLowerCase();
+    // Reverse the string
+    let reversed = str.split('').reverse().join('');
+    // Compare the original string with the reversed string
+    return str === reversed;
 
 }
 module.exports = palindrome
